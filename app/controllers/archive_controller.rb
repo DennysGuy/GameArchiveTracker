362b35fe68e90854d948ae017dda6b47b494d696
DennysGuy <mattmatriciano@gmail.com>
2024-06-18 13:03:15 -0700

    added skeleton pages

--- app/controllers/archive_controller.rb
+++ app/controllers/archive_controller.rb
@@ -1,4 +1,11 @@
 class ArchiveController < ApplicationController
   def index
   end
+
+  def checkout
+  end
+
+  def search
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   get 'archive/index'
+  get 'archive/checkout'
+  get 'archive/search'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
