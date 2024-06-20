29caf08f15d9cee5646c16d20e9e6c2b02356879
DennysGuy <mattmatriciano@gmail.com>
2024-06-19 17:50:13 -0700

    more navbar rearranging, added wishlist page

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   get 'archive/checkout'
   get 'archive/search'
   get 'archive/collections'
+  get 'archive/wishlist'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
